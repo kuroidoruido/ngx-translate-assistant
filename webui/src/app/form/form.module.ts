e8c638ca24863a088a55f8a7d11de29a35208c11
Anthony Pena <anthony.pena@outlook.fr>
2020-07-23 19:56:32 +0200

    feature - add contextual menus
    resolve #11

--- webui/src/app/form/form.module.ts
+++ webui/src/app/form/form.module.ts
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatTableModule } from '@angular/material/table';
 
 import { FormComponent } from './form/form.component';
@@ -23,6 +24,7 @@ import { MatDialogModule } from '@angular/material/dialog';
         MatExpansionModule,
         MatIconModule,
         MatInputModule,
+        MatMenuModule,
         MatTableModule,
     ],
     exports: [FormComponent],
